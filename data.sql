cdb724618acf111a14387e0954ec1c08fc9b54bb
yurat <ytatisch@gmail.com>
2015-08-22 05:37:36 +0000

    added databas

---
+++ data.sql
@@ -0,0 +1,5 @@
+Drop Table info;
+
+Create Table Info (id Integer Primary Key AUTO_INCREMENT, info Text);
+
+Insert Into info (info) Values("This id data");
--- index.php
+++ index.php
@@ -5,5 +5,30 @@
     
     <body>
         <h1>My PHP</h1>
+        
+        <?php
+            $servername = getenv("IP");
+            $username = getenv("C9_USER");
+            $password = "";
+            $database = "c9";
+            $dbport = 3306;
+            
+            $db = new mysqli ($servername, $username, $password, $databse, $dbport);
+            
+            if ($db->connect_error) {
+                die("Connection failed: " . $db->connect_error);
+            }
+            else {
+                echo "Connection OK (".$db->host_info.")";
+            }
+            
+            $query = "Select * Info";
+            
+            $result = mysqli_query($db, $query);
+            
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "The ID is: " . $row['id'];
+            }
+        ?>
     </body>
 </html>
